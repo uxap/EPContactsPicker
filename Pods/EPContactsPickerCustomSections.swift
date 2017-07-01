b2cd9e85e9da101bd6167bfff63bde5175071291
Eddie Lau <eddie@touchutility.com>
2017-07-01 12:20:59 +0800

    Added missing file.

---
+++ Pods/EPContactsPickerCustomSections.swift
@@ -0,0 +1,15 @@
+//
+//  EPContactsPickerCustomSections.swift
+//  Pods
+//
+//  Created by Eddie Hiu-Fung Lau on 1/7/2017.
+//
+//
+
+import Foundation
+
+public protocol EPContactsPickerCustomSections: UITableViewDataSource, UITableViewDelegate {
+    func setup(tableView:UITableView)
+}
+
+
